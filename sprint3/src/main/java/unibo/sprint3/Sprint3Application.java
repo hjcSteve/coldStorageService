600314c995337db77db998d253a5cc0c39149dcb
FloweroCoder <FloweroCoder@gihub.com>
2024-05-07 17:14:30 +0200

    sprint2 documento

---
+++ sprint3/src/main/java/unibo/sprint3/Sprint3Application.java
@@ -0,0 +1,13 @@
+package unibo.sprint3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sprint3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sprint3Application.class, args);
+	}
+
+}
---
+++ sprint3/src/test/java/unibo/sprint3/Sprint3ApplicationTests.java
@@ -0,0 +1,13 @@
+package unibo.sprint3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sprint3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

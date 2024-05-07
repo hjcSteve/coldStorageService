c5b2cbb315d801c2ab34e41c2c5c8b94c107c126
hjcSteve <hustefano99@gmail.com>
2024-05-07 18:05:44 +0200

    deployment dello sprint1

---
+++ sprint1V2/deploy.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+#mi sposto nella cartella del progetto
+# sudo chmod +x ./deploy.sh
+#faccio il build del progetto
+./gradlew clean build
+#rinomino la cartella libs in lib :) 
+mv build/libs build/lib
+#faccio il build dell'immagine docker e la taggo con il nome hjcsteve/coldstorageservice:1.2
+docker build -t hjcsteve/coldstorageservice:1.2 .
+#faccio il push dell'immagine su dockerhub di steve :) 
+#se non avete le credenziali non funziona
+docker push hjcsteve/coldstorageservice:1.2

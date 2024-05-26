7a6eab7de6fd5582caeb25759e3b40d76819d6c4
FloweroCoder <FloweroCoder@gihub.com>
2024-05-26 10:58:59 +0200

    ciao

---
+++ sprint0/sprint0V2/test/it/unibo/ctxcoldstorageservice/MainRobotTest.kt
@@ -0,0 +1,54 @@
+package test.it.unibo.ctxcoldstorageservice
+
+import org.junit.Assert.assertTrue
+import org.junit.BeforeClass
+import org.junit.Test
+import unibo.basicomm23.utils.CommUtils
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.net.Socket
+import unibo.basicomm23.tcp.TcpClientSupport
+
+class MainRobotTest {
+
+    val port=8090;
+    companion object {
+        @JvmStatic
+        @BeforeClass
+        fun setUp(): Unit {
+            println("set up");
+        }
+        val conn = TcpClientSupport.connect("localhost",8020,5)
+    }
+
+    @Test
+    fun ` test virtualrobot`(){
+        //mandiamo la request
+        val step = CommUtils.buildRequest("tester", "engage", "engage(transporttrolley,125)", "basicrobot").toString()
+        println(step);
+        val responseMessage = conn.request(step)
+        println(responseMessage)
+    }
+    @Test
+    fun ` test move`(){
+        //mandiamo la request
+        val step = CommUtils.buildRequest("tester", "moverobot", " moverobot(0,4)", "basicrobot").toString()
+        println(step);
+        val responseMessage = conn.request(step)
+        println(responseMessage)
+
+    }
+    @Test
+    fun ` test move home`(){
+        //mandiamo la request
+        val step = CommUtils.buildRequest("tester", "moverobot", " moverobot(0,0)", "basicrobot").toString()
+        println(step);
+        val responseMessage = conn.request(step)
+        println(responseMessage)
+
+    }
+
+
+
+}
